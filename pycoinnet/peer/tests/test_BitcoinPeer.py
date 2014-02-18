4ac6eb2e53fce87a5b00aa8c25a40236c3c1d9a4
Richard Kiss <him@richardkiss.com>
2014-02-18 14:29:49 -0800

    Another test.

--- pycoinnet/peer/tests/test_BitcoinPeer.py
+++ pycoinnet/peer/tests/test_BitcoinPeer.py
@@ -55,6 +55,15 @@ VERSION_MSG = dict(
 
 VERACK_MSG_BIN = b'foodverack\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00]\xf6\xe0\xe2'
 
+VERSION_MSG_2 = dict(
+    version=70001, subversion=b"/Notoshi/", services=1, timestamp=1392760614,
+    remote_address=PeerAddress(1, "127.0.0.1", 6111),
+    local_address=PeerAddress(1, "127.0.0.2", 6111),
+    nonce=5412937754643071,
+    last_block_index=0
+)
+
+
 def test_BitcoinPeerProtocol_send():
     DATA = []
     def write_f(data):
@@ -101,12 +110,8 @@ def test_BitcoinPeerProtocol_read():
     asyncio.get_event_loop().run_until_complete(async_test())
 
 def test_BitcoinPeerProtocol_multiplex():
-    DATA = []
-    def write_f(data):
-        DATA.append(data)
-
     peer = BitcoinPeerProtocol(MAGIC_HEADER)
-    pt = PeerTransport(write_f)
+    pt = PeerTransport(None)
     peer.connection_made(pt)
 
     next_message_list = [peer.new_get_next_message_f() for i in range(50)]
@@ -129,23 +134,37 @@ def test_BitcoinPeerProtocol_multiplex():
     assert COUNT == 50
 
 
-def ztest_BitcoinPeerProtocol():
-    def do_test(peer):
-        print("send getaddr to %s" % peer)
-        yield from perform_handshake(peer)
+def test_BitcoinPeerProtocol():
+    @asyncio.coroutine
+    def do_test(peer, vm1, vm2):
+        next_message = peer.new_get_next_message_f()
+
+        peer.send_msg("version", **vm1)
+        message_name, data = yield from next_message()
+        assert message_name == 'version'
+        assert data == vm2
+
+        peer.send_msg("verack")
+        message_name, data = yield from next_message()
+        assert message_name == 'verack'
+        assert data == {}
+
         peer.send_msg("getaddr")
-        
+        message_name, data = yield from next_message()
+        assert message_name == 'getaddr'
+        assert data == {}
+
     peer1 = BitcoinPeerProtocol(MAGIC_HEADER)
     peer2 = BitcoinPeerProtocol(MAGIC_HEADER)
 
-    pt1 = PeerTransport(peer2)
-    pt2 = PeerTransport(peer1)
+    pt1 = PeerTransport(peer2.data_received)
+    pt2 = PeerTransport(peer1.data_received)
 
     # connect them
     peer1.connection_made(pt2)
     peer2.connection_made(pt1)
 
-    f1 = asyncio.Task(perform_handshake(peer1))
-    f2 = asyncio.Task(perform_handshake(peer2))
+    f1 = asyncio.Task(do_test(peer1, VERSION_MSG, VERSION_MSG_2))
+    f2 = asyncio.Task(do_test(peer2, VERSION_MSG_2, VERSION_MSG))
 
     asyncio.get_event_loop().run_until_complete(asyncio.wait([f1, f2]))
