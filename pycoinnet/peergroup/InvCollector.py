766ecdbac9181da44fbe5b0031af78794010b24a
Richard Kiss <him@richardkiss.com>
2014-02-22 23:21:34 -0800

    Checkpoint for InvCollector.

--- pycoinnet/peergroup/InvCollector.py
+++ pycoinnet/peergroup/InvCollector.py
@@ -1,85 +1,109 @@
 """
-Provide InvCollector which has a "download_inv_item" method.
+InvCollector.py
+
+Listen to peers and queue when new InvItem objects are seen.
+
+Allow them to be fetched.
+
+Advertise objects that are fetched (to peers that haven't told us they have it).
+
+- queue of InvItem objects
+- fetch
+- advertise
 """
 
 import asyncio
 import logging
+import time
 
+from pycoinnet.InvItem import InvItem, ITEM_TYPE_TX, ITEM_TYPE_BLOCK
+from pycoinnet.peer.Fetcher import Fetcher
 from pycoinnet.util.Queue import Queue
-
-
-ITEM_TYPE_TX, ITEM_TYPE_BLOCK = (1, 2)
 
 
 class InvCollector:
 
     def __init__(self):
-        self.new_inv_item_tx_queue = Queue()
-        self.new_inv_item_block_queue = Queue()
-
-        ## keys: InvItem objects; values: weakref.WeakSet of peers
         self.inv_item_db = {}
+        # key: InvItem; value: weakref.WeakSet of peers
 
-    def handle_msg_inv(self, peer, items, **kwargs):
-        logging.debug("inv from %s : %d items", peer, len(items))
-        for item in items:
-            if item not in self.inv_item_db:
-                # it's new!
-                self.inv_item_db[item] = Queue()
-                if item.item_type == ITEM_TYPE_TX:
-                    self.new_inv_item_tx_queue.put_nowait(item)
-                elif item.item_type == ITEM_TYPE_BLOCK:
-                    self.new_inv_item_block_queue.put_nowait(item)
-            self.inv_item_db[item].put_nowait(peer)
+        self.disinterested_inv_item_set = set()
+
+        self.fetchers_by_peer = {}
+        self.advertise_q_for_peer = {}
+
+        self.new_inv_item_queue = Queue()
+
+    def add_peer(self, peer):
+        self.fetchers_by_peer[peer] = Fetcher(peer, ITEM_TYPE_TX)
+        self.advertise_q_for_peer[peer] = Queue()
+
+        @asyncio.coroutine
+        def _advertise_to_peer(peer):
+            while True:
+                items = []
+                while True:
+                    inv_item = yield from self.advertise_q_for_peer[peer]
+                    if peer not in self.inv_item_db[inv_item]:
+                        items.add(inv_item)
+                    if self.advertise_q_for_peer.qsize() == 0:
+                        break
+                # advertise the presence of the item!
+                if len(items) > 0:
+                    peer.send_msg("inv", items=items)
+
+        @asyncio.coroutine
+        def _watch_peer(peer, next_message, advertise_task):
+            try:
+                while True:
+                    name, data = yield from next_message()
+                    logging.debug("_watch_peer got %s %s", name, data)
+                    for inv_item in data["items"]:
+                        logging.debug("noting %s available from %s", inv_item, peer)
+                        self._register_inv_item(inv_item, peer)
+            except EOFError:
+                del self.fetchers_by_peer[peer]
+                advertise_task.cancel()
+
+        advertise_task = asyncio.Task(self._advertise_to_peer(peer))
+
+        next_message = peer.new_get_next_message_f(lambda name, data: name == "inv")
+        asyncio.Task(self._watch_peer(peer, next_message, advertise_task))
 
     @asyncio.coroutine
-    def next_new_tx_inv_item(self):
-        v = yield from self.new_inv_item_tx_queue.get()
-        return v
-
-    @asyncio.coroutine
-    def next_new_block_inv_item(self):
-        v = yield from self.new_inv_item_block_queue.get()
-        return v
-
-    @asyncio.coroutine
-    def download_inv_item(self, inv_item, peer_timeout=15):
-        q = self.inv_item_db.get(inv_item)
-        if not q:
-            logging.error("%s is unknown", inv_item)
-            return
-        peers_tried = set()
-        peers_we_can_try = set()
+    def fetch(self, inv_item, peer_timeout=10):
+        logging.debug("launched task to fetch %s", inv_item)
         while True:
-            if q.qsize() > 0:
-                more_peers = yield from q.get_all()
-                peers_we_can_try.update(more_peers)
-
-            peers_we_can_try.difference_update(peers_tried)
-            if len(peers_we_can_try) == 0:
-                peer = yield from q.get()
-                peers_we_can_try.add(peer)
+            the_set = self.inv_item_db[inv_item.data]
+            if len(the_set) == 0:
+                logging.error("couldn't find a place from which to fetch %s", inv_item)
+                del self.inv_item_db[inv_item.data]
+                return
+            peer = the_set.pop()
+            logging.debug("trying to fetch %s from %s, timeout %s", inv_item, peer, self.peer_timeout)
+            fetcher = self.fetchers_by_peer.get(peer)
+            if not fetcher:
+                logging.debug("no fetcher for %s", peer)
                 continue
+            item = yield from fetcher.fetch(inv_item.data, timeout=self.peer_timeout)
+            if item:
+                return item
 
-            # pick a peer. Eventually, by reputation, or some other
-            # metric. For now, we just do it randomly
-            peer = peers_we_can_try.pop()
-            peers_tried.add(peer)
+    def advertise_item(self, inv_item):
+        for q in self.advertise_q_for_peer.values():
+            q.put_nowait(inv_item)
 
-            logging.debug("queuing for fetch %s from %s", inv_item, peer)
+    def add_disinterested(self, disinterested_inv_item):
+        self.disinterested_inv_item_set.add(disinterested_inv_item)
 
-            future = yield from wait1(peer.request_inv_item(inv_item), timeout=peer_timeout)
-            if future.done():
-                return future.result()
+    def remove_disinterested(self, disinterested_inv_item):
+        self.disinterested_inv_item_set.discard(disinterested_inv_item)
 
-
-def wait1(future, timeout=None):
-    done, pending = yield from asyncio.wait([future], timeout=timeout)
-    if len(done) > 0:
-        future = done.pop()
-        exc = future.exception()
-        if exc:
-            raise exc
-    else:
-        future = pending.pop()
-    return future
+    def _register_inv_item(self, peer, inv_item):
+        logging.debug("inv from %s : %d items", peer, len(items))
+        the_hash = inv_item.data
+        if the_hash not in self.inv_item_db:
+            # it's new!
+            self.inv_item_db[the_hash] = {}
+            self.new_inv_item_queue.put_nowait(inv_item)
+        self.inv_item_db[the_hash][peer] = time.time()
