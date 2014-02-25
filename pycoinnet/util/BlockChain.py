44015a834545b8084ca91282d274fa1831918321
Richard Kiss <him@richardkiss.com>
2014-02-24 16:09:10 -0800

    Test of Blockchain rewrite.

--- pycoinnet/util/BlockChain.py
+++ pycoinnet/util/BlockChain.py
@@ -8,6 +8,117 @@ from pycoin.serialize import b2h_rev
 from pycoinnet.util.ChainFinder import ChainFinder
 from pycoinnet.util.Queue import Queue
 
+ZERO_HASH = b'\0' * 32
+
+
+def _update_q(q, ops):
+    # first, we meld out complimentary adds and removes
+    while len(ops) > 0:
+        op = ops[0]
+        if op[0] != 'remove':
+            break
+        last = q.pop()
+        if op[1:] != last[1:]:
+            q.put_nowait(last)
+            break
+        ops = ops[1:]
+    for op in ops:
+        q.put_nowait(op)
+
+
+class BlockChain:
+    def __init__(self, parent_hash=ZERO_HASH):
+        self.parent_hash = parent_hash
+        self.hash_to_index_lookup = {}
+        self.weight_lookup = {}
+        self.chain_finder = ChainFinder()
+        self.change_queues = set() #weakref.WeakSet()
+        self._longest_chain_cache = None
+
+    def length(self):
+        return len(self._longest_local_block_chain())
+
+    def tuple_for_index(self, index):
+        longest_chain = self._longest_local_block_chain()
+        the_hash = longest_chain[-index-1]
+        parent_hash = self.parent_hash if index <= 0 else self._longest_chain_cache[-index]
+        weight = self.weight_lookup.get(the_hash)
+        return (the_hash, parent_hash, weight)
+
+    def hash_for_index(self, index):
+        return self._longest_local_block_chain()[-index-1]
+
+    def index_for_hash(self, the_hash):
+        return self.hash_to_index_lookup.get(the_hash)
+
+    def new_change_q(self):
+        q = Queue()
+        self.change_queues.add(q)
+        return q
+
+    def _longest_local_block_chain(self):
+        if self._longest_chain_cache is None:
+            max_weight = 0
+            longest = []
+            for chain in self.chain_finder.all_chains_ending_at(self.parent_hash):
+                weight = sum(self.weight_lookup.get(h, 0) for h in chain)
+                if weight > max_weight:
+                    longest = chain
+                    max_weight = weight
+            self._longest_chain_cache = longest[:-1]
+        return self._longest_chain_cache
+
+    def add_nodes(self, hash_parent_weight_tuples):
+        def iterate():
+            for h, p, w in hash_parent_weight_tuples:
+                self.weight_lookup[h] = w
+                yield h, p
+
+        old_longest_chain = self._longest_local_block_chain()
+
+        self.chain_finder.load_nodes(iterate())
+
+        self._longest_chain_cache = None
+        new_longest_chain = self._longest_local_block_chain()
+
+        if old_longest_chain and new_longest_chain:
+            old_path, new_path = self.chain_finder.find_ancestral_path(old_longest_chain[0], new_longest_chain[0])
+            old_path = old_path[:-1]
+            new_path = new_path[:-1]
+        else:
+            old_path = old_longest_chain
+            new_path = new_longest_chain
+        if old_path:
+            logging.debug("old_path is %s-%s", old_path[0], old_path[-1])
+        if new_path:
+            logging.debug("new_path is %s-%s", new_path[0], new_path[-1])
+
+        # return a list of operations:
+        # ("add"/"remove", the_hash, the_index)
+        ops = []
+        size = len(old_longest_chain)
+        for idx, h in enumerate(old_path):
+            op = ("remove", h, size-idx-1)
+            ops.append(op)
+            del self.hash_to_index_lookup[size-idx-1]
+        size = len(new_longest_chain)
+        for idx, h in reversed(list(enumerate(new_path))):
+            op = ("add", h, size-idx-1)
+            ops.append(op)
+            self.hash_to_index_lookup[size-idx-1] = h
+        for q in self.change_queues:
+            _update_q(q, ops)
+
+        return ops
+
+
+
+"""
+TODO:
+  - add lock_to_index
+"""
+
+
 
 """
 BlockChain
@@ -58,7 +169,7 @@ def _update_q(q, ops):
         q.put_nowait(op)
 
 
-class BlockChain(object):
+class BlockChainz(object):
     """
     This is a compound object that accepts blocks (or block headers) via add_items.
 
