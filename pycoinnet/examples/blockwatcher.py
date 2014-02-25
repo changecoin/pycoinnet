6c85df269751a5968c85a3512ceed8b2da80144d
Richard Kiss <him@richardkiss.com>
2014-02-24 18:20:55 -0800

    Flake8 fix-up.

--- pycoinnet/examples/blockwatcher.py
+++ pycoinnet/examples/blockwatcher.py
@@ -6,15 +6,12 @@ Custom bitcoin client
 
 import argparse
 import asyncio
-import binascii
 import logging
 import os
 
 from pycoinnet.InvItem import ITEM_TYPE_BLOCK
 
 from pycoinnet.util.BlockChain import BlockChain
-from pycoinnet.util.LocalDB_RAM import LocalDB
-from pycoinnet.util.PetrifyDB import PetrifyDB
 
 from pycoinnet.peer.BitcoinPeerProtocol import BitcoinPeerProtocol
 
@@ -37,58 +34,11 @@ from pycoinnet.PeerAddress import PeerAddress
 
 
 @asyncio.coroutine
-def download_blocks(block_chain, blockfetcher, change_q, blockdir, depth, fast_forward_block_index):
-
-    block_future_q = Queue()
-
-    def write_block_to_disk(block, block_index):
-        p = os.path.join(blockdir, "block-%06d-%s.bin" % (block_index, block.id()))
-        tmp_path = p + ".tmp"
-        f = open(tmp_path, "wb")
-        block.stream(f)
-        f.close()
-        os.rename(tmp_path, p)
-
-    @asyncio.coroutine
-    def block_write_loop(block_future_q):
-        while True:
-            block_future, block_index = yield from block_future_q.get()
-            yield from block_future.wait_for(block_future, timeout=None)
-            block = block_future.result()
-            write_to_disk(block, block_index)
-
-    def download(block_hash, block_index):
-        f = blockfetcher.get_block_future(block_hash, block_index)
-        block = yield from asyncio.wait_for(f, timeout=None)
-        logging.info("got block id %s", block.id())
-        asyncio.get_event_loop().run_in_executor(None, write_to_disk, block_index, block)
-
-    asyncio.Task(block_write_loop(block_future_q))
-
-    while True:
-        add_or_remove, block_hash, block_index = yield from change_q.get()
-        if add_or_remove == 'add':
-            if block_index > fast_forward_block_index:
-                if block_chain.has_full_block_for_hash(block_hash):
-                    future = asyncio.Future()
-                    future.set_result(block_chain.full_block_for_hash(block_hash))
-                else:
-                    # get it
-                    future = blockfetcher.get_block_future(block_hash, block_index)
-                block_future_q.add(future, block_index)
-            if block_index - depth < fast_forward_block_index:
-                continue
-            index = block_index - depth
-            the_block = block_chain.item_for_index(block_index-depth)
-            write_to_disk(index, the_block)
-            if block_index > fast_forward_block_index:
-                asyncio.Task(download(block_hash, block_index))
-
-@asyncio.coroutine
 def show_connection_info(connection_info_q):
     while True:
         verb, noun, peer = yield from connection_info_q.get()
         logging.info("connection manager: %s on %s", verb, noun)
+
 
 def write_block_to_disk(blockdir, block, block_index):
     p = os.path.join(blockdir, "block-%06d-%s.bin" % (block_index, block.id()))
@@ -98,9 +48,11 @@ def write_block_to_disk(blockdir, block, block_index):
     f.close()
     os.rename(tmp_path, p)
 
+
 def block_processor(change_q, blockfetcher, inv_collector, config_dir, blockdir, depth, fast_forward):
     # load the last processed block index
-    last_processed_block = 0  ## should load from disk
+    last_processed_block = 0
+    # TODO: should load from disk
     # HACK. We should go to disk to cache this
     block_q = Queue()
     last_processed_block = max(last_processed_block, fast_forward)
@@ -128,20 +80,30 @@ def block_processor(change_q, blockfetcher, inv_collector, config_dir, blockdir,
             block = yield from asyncio.wait_for(future, timeout=None)
             write_block_to_disk(blockdir, block, block_index)
 
+
 @asyncio.coroutine
-def run_peer(peer, fast_forward_add_peer, blockfetcher):
+def run_peer(peer, fast_forward_add_peer, blockfetcher, tx_mempool, inv_collector):
     yield from asyncio.wait_for(peer.connection_made_future, timeout=None)
     version_parameters = default_msg_version_parameters(peer)
     version_data = yield from initial_handshake(peer, version_parameters)
     last_block_index = version_data["last_block_index"]
     fast_forward_add_peer(peer, last_block_index)
     blockfetcher.add_peer(peer, last_block_index)
+    tx_mempool.add_peer(peer)
+    inv_collector.add_peer(peer)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Watch Bitcoin network for new blocks.")
     parser.add_argument('-c', "--config-dir", help='The directory where config files are stored.')
-    parser.add_argument('-f', "--fast-forward", type=int, help="block index to fast-forward to (ie. don't download full blocks prior to this one)", default=0)
-    parser.add_argument('-d', "--depth", type=int, help="Minimum depth blocks must be buried before being dropped in blockdir", default=2)
+    parser.add_argument(
+        '-f', "--fast-forward", type=int,
+        help="block index to fast-forward to (ie. don't download full blocks prior to this one)", default=0
+    )
+    parser.add_argument(
+        '-d', "--depth", type=int,
+        help="Minimum depth blocks must be buried before being dropped in blockdir", default=2
+    )
     parser.add_argument("blockdir", help='The directory where new blocks are dropped.')
 
     asyncio.tasks._DEBUG = True
@@ -150,34 +112,40 @@ def main():
         format=('%(asctime)s [%(process)d] [%(levelname)s] '
                 '%(filename)s:%(lineno)d %(message)s'))
     logging.getLogger("asyncio").setLevel(logging.INFO)
-    queue_of_timestamp_peeraddress_tuples = new_queue_of_timestamp_peeraddress_tuples(MAINNET)
-    if 0:
+    if 1:
+        queue_of_timestamp_peeraddress_tuples = new_queue_of_timestamp_peeraddress_tuples(MAINNET)
+    else:
         queue_of_timestamp_peeraddress_tuples = Queue()
         queue_of_timestamp_peeraddress_tuples.put_nowait((0, PeerAddress(1, "127.0.0.1", 28333)))
 
     args = parser.parse_args()
-    config_dir = args.config_dir
 
-    local_db = LocalDB()
-    petrify_db = PetrifyDB(dir_path=config_dir)
-    block_chain = BlockChain(local_db, petrify_db)
+    block_chain = BlockChain()
     change_q = block_chain.new_change_q()
     blockfetcher = Blockfetcher()
 
     inv_collector = InvCollector()
-    tx_mempool = TxMempool(inv_collector, is_interested_f=lambda inv_item: inv_item.item_type == ITEM_TYPE_BLOCK)
+    tx_mempool = TxMempool(
+        inv_collector,
+        is_interested_f=lambda inv_item: inv_item.item_type == ITEM_TYPE_BLOCK
+    )
 
-    asyncio.Task(block_processor(change_q, blockfetcher, inv_collector, args.config_dir, args.blockdir, args.depth, args.fast_forward))
+    asyncio.Task(
+        block_processor(
+            change_q, blockfetcher, inv_collector, args.config_dir,
+            args.blockdir, args.depth, args.fast_forward))
     fast_forward_add_peer = fast_forwarder_add_peer_f(block_chain)
 
     def create_protocol_callback():
         peer = BitcoinPeerProtocol(MAINNET["MAGIC_HEADER"])
         install_ping_manager(peer)
         install_pong_manager(peer)
-        asyncio.Task(run_peer(peer, fast_forward_add_peer, blockfetcher))
+        asyncio.Task(run_peer(peer, fast_forward_add_peer, blockfetcher, tx_mempool, inv_collector))
         return peer
 
-    connection_info_q = manage_connection_count(queue_of_timestamp_peeraddress_tuples, create_protocol_callback, 8)
+    connection_info_q = manage_connection_count(
+        queue_of_timestamp_peeraddress_tuples,
+        create_protocol_callback, 8)
     asyncio.Task(show_connection_info(connection_info_q))
     asyncio.get_event_loop().run_forever()
 
