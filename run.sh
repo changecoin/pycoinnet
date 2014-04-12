bdb15bbcaf793b9743c8d071163deff673f8fd6f
Richard Kiss <him@richardkiss.com>
2014-04-12 00:56:16 -0700

    Tweak run script.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-PYTHONPATH=`pwd` pycoinnet/examples/blockwatcher.py -s blockwatcher_state -f 288900 -l blockwatcher.log blockstore
+PYTHONPATH=`pwd` pycoinnet/examples/blockwatcher.py -s state_blockwatcher -f 288900 -l blockwatcher.log blockstore
