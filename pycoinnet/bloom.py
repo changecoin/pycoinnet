4a362c8e7a9b10a5d2297d561b03c538c911828d
Richard Kiss <him@richardkiss.com>
2014-10-26 15:17:54 -0700

    Merkle block tests working great now!

--- pycoinnet/bloom.py
+++ pycoinnet/bloom.py
@@ -1,6 +1,23 @@
+#The size S of the filter in bytes is given by (-1 / pow(log(2), 2) * N * log(P)) / 8. Of course you must ensure it does not go over the maximum size (36,000: selected as it represents a filter of 20,000 items with false positive rate of < 0.1% or 10,000 items and a false positive rate of < 0.0001%).
+
+#The number of hash functions required is given by S * 8 / N * log(2).
+
+import math
+
+LOG_2 = math.log(2)
+
+def filter_size_required(element_count, false_positive_probability):
+    return int(((-1 / pow(LOG_2, 2) * element_count * math.log(false_positive_probability))+7) // 8)
+
+
+def hash_function_count_required(filter_size, element_count):
+    return int(filter_size * 8 / element_count * LOG_2 + 0.5)
+
 
 class BloomFilter(object):
     def __init__(self, size_in_bytes, hash_function_count, tweak):
+        if size_in_bytes > 36000:
+            raise ValueError("too large")
         self.filter_bytes = bytearray(size_in_bytes)
         self.bit_count = 8 * size_in_bytes
         self.hash_function_count = hash_function_count
@@ -9,7 +26,7 @@ class BloomFilter(object):
     def add_item(self, item_bytes):
         for hash_index in range(self.hash_function_count):
             seed = hash_index * 0xFBA4C795 + self.tweak
-            self.set_bit(murmur3(item_bytes), seed=seed)
+            self.set_bit(murmur3(item_bytes, seed=seed) % self.bit_count)
 
     def _index_for_bit(self, v):
         v %= self.bit_count
@@ -22,7 +39,7 @@ class BloomFilter(object):
         self.filter_bytes[byte_index] |= mask
 
     def check_bit(self, v):
-        byte_index, mask = self._index_for_bit(self, v)
+        byte_index, mask = self._index_for_bit(v)
         return (self.filter_bytes[byte_index] & mask) == mask
 
     def filter_load_params(self):
--- pycoinnet/examples/merkle_test.py
+++ pycoinnet/examples/merkle_test.py
@@ -2,7 +2,7 @@ import asyncio
 import os
 
 from pycoin.encoding import double_sha256
-from pycoin.serialize import h2b_rev, b2h_rev
+from pycoin.serialize import b2h_rev, h2b, h2b_rev
 
 from pycoinnet.helpers import networks
 from pycoinnet.helpers.dnsbootstrap import dns_bootstrap_host_port_q
@@ -106,9 +106,11 @@ def t():
 
 def main():
     bf = BloomFilter(1024, hash_function_count=8, tweak=3)
-    for i in range(0, 1024*8):
-        if i % 4 != 3:
-            bf.set_bit(i)
+
+    if 0:
+        for i in range(0, 1024*8):
+            if i % 4 != 3:
+                bf.set_bit(i)
 
     network = networks.MAINNET
     host_port_q = dns_bootstrap_host_port_q(network)
@@ -133,16 +135,45 @@ def main():
     peer.send_msg("mempool")
     m = yield_from(next_message())
 
+    # Test the hash of the transaction itself.
+    if 0:
+        tx_hash = h2b_rev("3e5bff948585e292fa0b043b62de4019787ffdf872f6cb21e124c204d09be88a")
+        bf.add_item(tx_hash)
+
+    # For each output, test each data element of the output script. This means each hash and key in the output script is tested independently. Important: if an output matches whilst testing a transaction, the node might need to update the filter by inserting the serialized COutPoint structure. See below for more details.
+    if 1:
+        from pycoin.encoding import bitcoin_address_to_hash160_sec
+        address = "1bonesyuMYEZQBQmy4Z6N9gpoNHqx8Dev"
+        the_hash160 = bitcoin_address_to_hash160_sec(address)
+        bf.add_item(the_hash160)
+
+    # For each input, test the serialized COutPoint structure.
+    if 0:
+        # Spendable
+        the_c_out = (h2b_rev("3e5bff948585e292fa0b043b62de4019787ffdf872f6cb21e124c204d09be88a")) + b'\0\0\0\0'
+        bf.add_item(the_c_out)
+
+    #For each input, test each data element of the input script (note: input scripts only ever contain data elements).
+    # ?
+    if 1:
+        data_element = h2b("304402202b30dba17d6cc3926f6fb317509b68cb877f1287bc12cfc1168811071508bd74022065af95136076450ba6064b48b6ac583dd40a1c7d5d454c2a7faf4014a111112501")
+        bf.add_item(data_element)
+
+    #Otherwise there is no match.
+
     the_bytes, hfc, tweak = bf.filter_load_params()
     peer.send_msg("filterload", filter=the_bytes, hash_function_count=hfc, tweak=tweak, flags=0)
     h = h2b_rev("0000000000000000186831e8e00586e763deff7f6bc04436b0e988523f7fcf16")
     h = h2b_rev("0000000000bc76619da94d92e0fb82570141a827603c0f1dfb6fe06ee8c96c79")
-    #h = h2b_rev("00000000001a2edf34b50bedfa2332bc6d9f47ae9bde9b038d11469006d5b114")
+    h = h2b_rev("00000000000000000fe003fb653d25916a0c58bf952c5412664e5fa7063761fd")
 
     items = [InvItem(ITEM_TYPE_MERKEL_BLOCK, h)]
     peer.send_msg("getdata", items=items)
-    m = yield_from(next_message())
-    print(m)
+    while 1:
+        m = yield_from(next_message())
+        print(m)
+        if m[0] == "merkleblock":
+            break
     txs = tx_hashes_for_merkle_message(m)
     print(txs)
     while 1:
